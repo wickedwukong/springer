103a5bc4ede5b64d664a2a165a69f1f70cf1ce0d
Xuemin Guan <xuemin.guan@db.com>
2014-02-03 17:17:03 +0000

    removing a bit of dead code.

--- src/main/scala/springer/interview/ToDrawing.scala
+++ src/main/scala/springer/interview/ToDrawing.scala
@@ -27,31 +27,3 @@ object SupportedCommand {
     }
   }
 }
-
-
-object toDrawing {
-  import Drawings._
-
-  def apply(rawInstruction: String):  Canvas => Canvas = {
-      rawInstruction.split(" ").toList match {
-        case "C" :: tail => {
-          val canvasDimension: List[Int] = tail.take(2).map(_.toInt)
-          canvasDrawing(canvasDimension(0), canvasDimension(1))
-        }
-        case "L" :: tail => {
-          val lineStartAndEnd: List[Int] = tail.take(4).map(_.toInt)
-          lineDrawing(lineStartAndEnd(0), lineStartAndEnd(1), lineStartAndEnd(2), lineStartAndEnd(3))
-        }
-        case "R" :: tail => {
-          val topLeftCornerAndBottomRightCorner: List[Int] = tail.take(4).map(_.toInt)
-
-          rectangleDrawing(topLeftCornerAndBottomRightCorner(0), topLeftCornerAndBottomRightCorner(1), topLeftCornerAndBottomRightCorner(2), topLeftCornerAndBottomRightCorner(3))
-        }
-        case "B" :: tail => {
-          val color = tail(2).head
-          val startingPoint: List[Int] = tail.take(2).map(_.toInt)
-          bucketFill(startingPoint(0), startingPoint(1), color)
-        }
-    }
-  }
-}
